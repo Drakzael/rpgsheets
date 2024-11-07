a98a06a5eb24d43df90bf47bb94d17287030afee
Drakzael <drakzael@gmail.com>
2024-11-07 18:57:51 +0100

    Auto logout on 401 error.

--- client/src/app/interceptor/jwt.interceptor.ts
+++ client/src/app/interceptor/jwt.interceptor.ts
@@ -1,17 +1,24 @@
 import { inject } from '@angular/core';
-import { HttpRequest, HttpEvent, HttpInterceptorFn, HttpHandlerFn } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpRequest, HttpEvent, HttpInterceptorFn, HttpHandlerFn, HttpErrorResponse } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
 import { AccountService } from '../_services/account.service';
+import { Router } from '@angular/router';
 
 export const jwtInterceptor: HttpInterceptorFn = (request: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> => {
-    // add auth header with jwt if user is logged in and request is to the api url
-    const accountService = inject(AccountService);
-    const user = accountService.userValue;
-    const isLoggedIn = user && user.token;
-    const isApiUrl = request.url.startsWith("/api");
-    if (isLoggedIn && isApiUrl) {
-        request = request.clone({ setHeaders: { Authorization: `Bearer ${user.token}` } });
-    }
-
-    return next(request);
+  // add auth header with jwt if user is logged in and request is to the api url
+  const accountService = inject(AccountService);
+  const user = accountService.userValue;
+  const isLoggedIn = user && user.token;
+  const isApiUrl = request.url.startsWith("/api");
+  if (isLoggedIn && isApiUrl) {
+    request = request.clone({ setHeaders: { Authorization: `Bearer ${user.token}` } });
+  }
+  return next(request).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        accountService.logout();
+      }
+      return throwError(error);
+    })
+  );
 }
